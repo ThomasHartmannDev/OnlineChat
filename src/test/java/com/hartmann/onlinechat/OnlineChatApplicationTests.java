42e3ceb774c9df9c40c6f3430602e0efeb7e092e
Thomas Hartmann <t.hartmann@equant.ch>
2025-10-30 23:40:21 +0100

    Initial commit

---
+++ src/test/java/com/hartmann/onlinechat/OnlineChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hartmann.onlinechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
